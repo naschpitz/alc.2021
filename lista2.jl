756e9c590e9ab9d19f8bcdb68c19e9d1848318b8
fabiocsirj <fabiocsirj@gmail.com>
2021-07-02 15:46:51 -0300

    Lista 3 Numero 8

--- lista2.jl
+++ lista2.jl
@@ -1,9 +1,9 @@
 using LinearAlgebra
 include("plots.jl")
 
-function produtoInterno_Angulo(v, u)                                        # Número 9 da Lista 2.
+function produtoInterno_Angulo(v, u)                                        # Número 9 da Lista 2. (Fábio)
     if size(v)[1] != 2 || size(u)[1] != 2                                   # Para satisfazer a letra (c)...
-        println("Os vetores devem morar em R^2!")                            # ...testamos se os vetores pertencem a R^2.
+        println("Os vetores devem morar em R^2!")                           # ...testamos se os vetores pertencem a R^2.
     else
         if v[1] < 0 || v[2] < 0 || u[1] < 0 || u[2] < 0                     # Testa se todos os valores (dos vetores v e u)...
             print("Os vetores devem ser não-negativos!")                    # ...não são menores que 0 (não-negativos).
@@ -27,4 +27,31 @@ function produtoInterno_Angulo(v, u)                                        # N�
             annotate!(p[1], p[2], text("$(angulo)º", 10))                   # Imprime o ângulo.
         end
     end
+end
+
+function lago_fabio(s, v)                                           # Número 8 da Lista 3. (Fábio)
+    A = zeros(s*s, s*s)                                             # "v" é uma vetor de 4 posições, com as temperaturas das bordas
+    b = zeros(s*s)                                                  # s -> parametro q informa a raiz da quantidade...
+    for i = 1:s*s                                                   # ...de pontos a se medir (s^2) = Matriz s x s
+        for j = 1:s*s                                               # Este "for" preenche a Matriz A de tamanho s^2 x s^2
+            i==j ? A[i,j] = 1 : nothing                             # Preenche com valor 1 na diagonal principal
+            j>i && (i%s)!=0 && j==i+1 ? A[i,j] = -0.25 : nothing    # Preenche com -0.25 na lateral direita do 1 se este não estiver na borda direita 
+            j<i && (i%s)!=1 && j==i-1 ? A[i,j] = -0.25 : nothing    # Preenche com -0.25 na lateral esquerda do 1 se este não estiver na borda esquerda
+            j==i+s || j==i-s ? A[i,j] = -0.25 : nothing             # Preenche com -0.25 acima e abaixo do 1
+        end
+                                                                    # Daqui para baixo preenche o vetor "b"
+        if i<=s                                                     # Borda superior = v[2]
+            b[i] = v[2]/4                                           # Inicialmente é o valor da borda superior/4
+            i==1 ? b[i] = v[1]/4 + v[2]/4 : nothing                 # Se está na quina esquerda, é borda superior/4 + esquerda/4
+            i==s ? b[i] = v[2]/4 + v[3]/4 : nothing                 # Se está na quina direita, é borda superior/4 + direita/4
+        elseif i>(s*s)-s                                            # Borda inferior = v[4]
+            b[i] = v[4]/4                                           # Inicialmente é o valor da borda inferior/4
+            i==(s*s)-s+1 ? b[i] = v[1]/4 + v[4]/4 : nothing         # Se está na quina esquerda, é borda inferior/4 + esquerda/4
+            i==s*s ? b[i] = v[3]/4 + v[4]/4 : nothing               # Se está na quina direita, é borda inferior/4 + direita/4
+        else                                                        # Bordas laterais (exceto quina superior e quina inferior)...
+            (i%s)==1 ? b[i] = v[1]/4 : nothing                      # ...laterais esquerda = v[1]/4
+            (i%s)==0 ? b[i] = v[3]/4 : nothing                      # ...laterais direita  = v[3]/4
+        end
+    end
+    return A\b                                                      # x = A\b
 end
--- teste.jl
+++ teste.jl
@@ -1,7 +1,13 @@
 include("lista2.jl")
 
-# Testando a Função da Lista 2 Número 9:
+# (Fábio) Testando a Função da Lista 2 Número 9:
 v1 = [5;5]
 v2 = [5;0]
 produtoInterno_Angulo(v1, v2)
-########################################
+################################################
+
+# (Fábio) Testando a Função da Lista 3 Número 8:
+temp_bordas  = [25; 20; 20; 30]
+quant_pontos = 25
+lago_fabio(Int(sqrt(quant_pontos)), temp_bordas)
+################################################
