7bb7a8f6aeb8f7a2b90ec9ddc1159762a8c67385
fabiocsirj <fabiocsirj@gmail.com>
2021-07-05 00:59:35 -0300

    Lista 5 Número 2

--- lista5.jl
+++ lista5.jl
@@ -1,3 +1,6 @@
+using LinearAlgebra
+include("plots.jl")
+
 function getMatrix(eigVecs, eigVals)
     n = length(eigVals)
     
@@ -11,4 +14,37 @@ function getMatrix(eigVecs, eigVals)
     M = V*D*inv(V)
     
     return M
+end
+
+function transformações_RPQ(M, T1, T2=nothing)                  # Número 2 da Lista 5. (Fábio)
+    v1 = M[:,1]                                                 # Pega o primeiro vetor e armazena em v1
+    v2 = M[:,2]                                                 # Pega o segundo vetor e armazena em v2
+    if dot(v1, v2) != 0                                         # Verifica se são ortogonais...
+        println("Vetores não são ortogonais!!!")                # ...Senão for, imprime mensagem de erro e termina
+    else                                                        # ...Se for continua...
+        A1 = T1 * v1                                            # Aplica primeira transformação no primeiro vetor
+        vel1 = 0.02*(v1-A1)                                     # Calibra velocidade da animação
+        n = 51
+
+        if  T2 !== nothing                                      # Se houver uma segunda transformação...
+            A2 = T2 * v2                                        # Faz os cálculos pra ela também
+            vel2 = 0.02*(v2-A2)
+            n = 101
+        end
+        
+        @gif for i in 1:n                                       # Anima a transformação...
+            M = [v1 v2]
+            if i < 52
+                plano_cartesiano(M)
+                annotate!(M[1,1], M[2,1]-1, text("r", 12))
+                annotate!(M[1,2], M[2,2]-1, text("s", 12))
+                v1 -= vel1
+            else
+                plano_cartesiano(M)
+                annotate!(M[1,1], M[2,1]-1, text("r", 12))
+                annotate!(M[1,2], M[2,2]-1, text("s", 12))
+                v2 -= vel2
+            end
+        end
+    end
 end
--- teste.jl
+++ teste.jl
@@ -28,73 +28,88 @@ include("lista5.jl")
 # (Bruno) Testando a Função da Lista 1 Número 2
 
 #Testando com valores B e C de questões anteriores.
-B = [1 3 5; 4 5 9; 3 5 8]
-C = [1 0 0 0 1; 0 1 0 0 10; 0 0 1 1000 0]
+# B = [1 3 5; 4 5 9; 3 5 8]
+# C = [1 0 0 0 1; 0 1 0 0 10; 0 0 1 1000 0]
 
-E, F = getEF(B, C)
+# E, F = getEF(B, C)
 
-println(B*C == E*F)
+# println(B*C == E*F)
 
 ################################################
 
 ################################################
 # (Bruno) Testando a Função da Lista 1 Número 3
 
-V = [1 1 11 01 11 2 13 2; 1 2 12 102 21 3 10 2; 1 3 13 103 31 4 7 2; 1 4 14 104 41 5 4 3]
+# V = [1 1 11 01 11 2 13 2; 1 2 12 102 21 3 10 2; 1 3 13 103 31 4 7 2; 1 4 14 104 41 5 4 3]
 
-B = getBase(V)
+# B = getBase(V)
 
-println(B)
+# println(B)
 
-anotherB = getAnotherBase(B)
+# anotherB = getAnotherBase(B)
 
-println(anotherB)
+# println(anotherB)
 
 ################################################
 
 ################################################
 # (Bruno) Testando a Função da Lista 2 Número 6
 
-V = [3 2; 1 2]
+# V = [3 2; 1 2]
 
-R = GS(V)
+# R = GS(V)
 
-println(R)
+# println(R)
 ################################################
 
 ################################################
 # (Bruno) Testando a Função da Lista 3 Número 8:
 #Teste com a matriz da letra "a"
 
-A = zeros(2, 2)
-b = [5; 15; 35; 10]
+# A = zeros(2, 2)
+# b = [5; 15; 35; 10]
 
-I, B = interpolate(A, b)
+# I, B = interpolate(A, b)
 
-println(I)
-println(B)
+# println(I)
+# println(B)
 ################################################
 
 ################################################
 # (Bruno) Testando a Função da Lista 4 Número 7:
 #Teste com a matriz da letra "a"
 
-V = [1000 300 6000; 4 6 11]
+# V = [1000 300 6000; 4 6 11]
 
-a = minQuad_retaH(V)
+# a = minQuad_retaH(V)
 
-println(a)
+# println(a)
 ################################################
 
 ################################################
 # (Bruno) Testando a Função da Lista 5 Número 5:
 #Teste com a matriz da letra "a"
 
-eigVecs = [1 1 -1; 0 1 2; -1 1 -1]
-eigVals = [1; -2; 2]
+# eigVecs = [1 1 -1; 0 1 2; -1 1 -1]
+# eigVals = [1; -2; 2]
 
-M = getMatrix(eigVecs, eigVals)
+# M = getMatrix(eigVecs, eigVals)
 
-println(M)
-println(M * [-1; 2; -1])
+# println(M)
+# println(M * [-1; 2; -1])
+################################################
+
+# (Fábio) Testando a Função da Lista 5 Número 2:
+r = [ 5, 5]         # Define r e s...
+s = [-5, 5]         # ...estes devem ser ortogonais
+Reflexo  = [-1  0;  # Define a matriz de transformação...
+             0 -1]  # ...que causa reflexão
+ProjOrto = [ 0  0;  # Define a matriz de transformação...
+             0  0]  # ...projeção ortogonal
+# transformações_RPQ(1, 2, 3) -> sendo:
+# 1: Matriz formada por dois vetores (colunas) ortogonais
+# 2: Função que transforma o primeiro vetor
+# 3: (opcional) função que transforma o segundo vetor
+transformações_RPQ([r s], Reflexo, ProjOrto)
+#idade_dos_amigos()
 ################################################
