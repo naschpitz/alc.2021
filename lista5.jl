d74b4e09bbeb3bafac3ec89cdbc3130b2cd7a387
fabiocsirj <fabiocsirj@gmail.com>
2021-07-07 12:46:34 -0300

    transformações_RPQ revisada

--- lista5.jl
+++ lista5.jl
@@ -20,14 +20,14 @@ function transformações_RPQ(M, T)                           # Número 2 da Lis
     if dot(M[:,1], M[:,2]) > 1e-8                           # Verifica se são ortogonais...
         println("Vetores não são ortogonais!!!")            # ...Senão for, imprime mensagem de erro e termina
     else                                                    # ...Se for continua...
-        A1 = M * T                                          # Aplica a transformação na matriz
-        vel1 = 0.02*(M-A1)                                  # Calibra velocidade da animação
+        A   = M * T                                          # Aplica a transformação na matriz
+        vel = 0.02*(M-A)                                  # Calibra velocidade da animação
         
         @gif for i in 1:51                                  # Anima a transformação...
             plano_cartesiano(M)
             annotate!(M[1,1], M[2,1]-1, text("r", 12))
             annotate!(M[1,2], M[2,2]-1, text("s", 12))
-            M -= vel1
+            M -= vel
         end
     end
 end
